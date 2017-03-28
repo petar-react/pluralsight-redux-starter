86c051d09f578fbf03fdb98cb67486b93ca50cc0
Marko Kresojevic <marko.k.89@gmail.com>
2017-03-28 17:34:21 +0200

    promene
    Promene

--- src/components/questions/QuestionCreatePage.js
+++ src/components/questions/QuestionCreatePage.js
@@ -1,5 +1,8 @@
 import React , {PropTypes} from 'react';
-//create question function
+import QuestionForm from './QuestionForm';
+
+
+
 class QuestionCreatePage extends React.Component {
   constructor () {
     super();
@@ -8,134 +11,18 @@ class QuestionCreatePage extends React.Component {
     };
   }
   render(){
-
-    const question = [];
-
-    // this.state.numChildren.map(children.push(<Answer number={children.length+1} />));
-    for (var i = 0; i < this.state.numQuestions; i += 1) {
-      question.push(<Question key={i} number={i} > </Question>);
-    }
-    //Question Form
     return(
       <div>
         <h1>Hello</h1>
 
-        <QuestionForm  addQuestion={this.onAddQuestion.bind(this)}>
-          {question}
-        </QuestionForm>
-      </div>
-    );
-  }
-//Add Question function
-  onAddQuestion () {
-    this.setState({
-      numQuestions: this.state.numQuestions + 1
-    });
-  }
+        <QuestionForm />
 
-
-}
-
-class QuestionForm extends React.Component {
-
-  render(){
-//field questions, On click Add Question
-    return(
-      <div className="card calculator">
-        <p><a href="#" onClick={this.props.addQuestion}>Add Another Question</a></p>
-        <div id="children-pane">
-          {this.props.children}
-        </div>
       </div>
     );
   }
 }
-/* submit and change */
-class Question extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-    this.addAnswer = this.addAnswer.bind(this);
-    this.state = {
-      numAnswers: 1
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render(){
-    const answer = [];
-
-    // this.state.numChildren.map(children.push(<Answer number={children.length+1} />));
-    for (var k = 0; k < this.state.numAnswers; k += 1) {
-      answer.push(<Answer key={k} number={k} />);
-    }
-    /* field answer, submit for Question, Add Answer on Click */
-    return(
-      <div className="panel-body">
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Question:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
 
 
-        <p><a href="#" onClick={this.addAnswer}>Add Another Answer</a></p>
-        <div>
-          {answer}
-        </div>
-      </div>
-
-
-    );
-  }
-
-//Add Answer function
-
-   addAnswer () {
-    this.setState({
-      numAnswers: this.state.numAnswers + 1
-    });
-  }
-}
-
-class Answer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-/* submit for Answer*/
-  render(){
-    return(
-      <div className="panel-body">
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Answer:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
-  }
-}
 
 
 
